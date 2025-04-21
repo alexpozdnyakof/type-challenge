f642a180c79ad83e5343836d172c5045131618dc
Aleksandr Pozdnyakov <alexpozdnyakof@gmail.com>
2025-04-21 15:45:13 +0700

    easy(11): tuple to object solution

--- easy/00011-easy-tuple-to-object.ts
+++ easy/00011-easy-tuple-to-object.ts
@@ -20,7 +20,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly PropertyKey[]> = {[I in T[number]]: I}
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
